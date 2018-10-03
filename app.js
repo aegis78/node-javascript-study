3a953b46b5c89bf0b76c7b96576d1e3159963cd3
yiseungju <kkan79@gmail.com>
2018-10-03 23:10:25 +0900

    node-20181003

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
 var app = express();
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.send('Hello World Express');
 });
 
+app.get('/dynamic', function(req, res) {
+    var output = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title></title>        
+        </head>
+        <body>
+            Hello Static
+        </body>
+    </html>    
+    `
+    res.send(output);
+});
 
 app.get('/login', (req, res) => {
     res.send('Login please');
